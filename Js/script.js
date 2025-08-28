2354ab6da860e9a03106efe88ddd6c03babbfe22
sumi17code <sumiakter17@gmail.com>
2025-08-28 18:04:30 +0530

    js added

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,29 @@
 
+let coinCount = 100;
+    let heartCount = 0;
+    let copyCount = 0;
+
+    const coinCountEl = document.getElementById("coinCount");
+    const heartCountEl = document.getElementById("heartCount");
+    const copyCountEl = document.getElementById("copyCount");
+    const historyList = document.getElementById("historyList");
+
+// Increase heart count
+
+    function increaseHeart() {
+      heartCount++;
+      heartCountEl.textContent = heartCount;
+    }
+
+// copy number 
+    function copyNumber(number) {
+      navigator.clipboard.writeText(number).then(() => {
+        alert(" Number copied: " + number);
+        copyCount++;
+        copyCountEl.textContent = copyCount;
+      });
+    }
+
+    
+
+
